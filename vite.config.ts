23885f2a515a1a34abf6e30caa97e54c93f0fde6
AlyairHB <alyairhuerta@gmail.com>
2026-03-13 22:09:55 -0600

    2do intento de prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3001, // Puerto para desarrollo (cambiado para no chocar con backend)
+  },
 });
